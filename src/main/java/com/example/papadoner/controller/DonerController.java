46115171d9ecb7276d55466f070dc5f6151f239a
OlegTx2OB <olegtx2ob@gmail.com>
2024-03-20 11:32:37 +0300

    end1st lab

--- src/main/java/com/example/papadoner/controller/DonerController.java
+++ src/main/java/com/example/papadoner/controller/DonerController.java
@@ -1,7 +1,7 @@
 package com.example.papadoner.controller;
 
-import com.example.papadoner.dao.DonerDAO;
 import com.example.papadoner.model.Doner;
+import com.example.papadoner.service.DonerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,16 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DonerController {
 
-    private DonerDAO donerDAO;
+    private final DonerService donerService;
 
     @Autowired
-    public DonerController(DonerDAO donerDAO) {
-        this.donerDAO = donerDAO;
+    public DonerController(DonerService donerService) {
+        this.donerService = donerService;
     }
 
     @GetMapping("/getDoner/{name}")
     public Doner getDonerByName(@PathVariable("name") String name) {
-        return donerDAO.findByName(name);
+        return donerService.getDonerByName(name);
     }
 
 }
---
+++ src/main/java/com/example/papadoner/service/DonerService.java
@@ -0,0 +1,8 @@
+package com.example.papadoner.service;
+
+import com.example.papadoner.model.Doner;
+
+public interface DonerService {
+
+    Doner getDonerByName(String name);
+}
