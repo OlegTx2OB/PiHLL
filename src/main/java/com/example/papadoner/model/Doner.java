3cab923f2520533e3ef24ea20b7a61e43e21ad67
OlegTx2OB <olegtx2ob@gmail.com>
2024-03-26 19:01:39 +0300

    2lab ended

--- src/main/java/com/example/papadoner/model/Doner.java
+++ src/main/java/com/example/papadoner/model/Doner.java
@@ -22,15 +22,8 @@ public class Doner {
     private String name;
 
     @ManyToMany
-    @JoinTable(name = "doner_ingredients",
-            joinColumns = @JoinColumn(name = "doner_id"),
-            inverseJoinColumns = @JoinColumn(name = "ingredients_id"))
     private List<Ingredient> ingredients;
 
-
     @ManyToMany
-    @JoinTable(name = "doner_price_by_weights",
-            joinColumns = @JoinColumn(name = "doner_id"),
-            inverseJoinColumns = @JoinColumn(name = "price_by_weights_id"))
     private Set<PriceByWeight> priceByWeights;
 }
